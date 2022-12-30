989522677fc6ab8fa6a99a89ec8e20a4f54f1d24
RaviSenjaliya <109456574+RaviSenjaliya@users.noreply.github.com>
2022-12-30 09:41:52 +0530

    this data

---
+++ demo/src/component/ReduxThunk/ThunkCounter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { plus, Minus, AsncMinus } from "./Action/ActionT";
+
+export default function ThunkCounter() {
+  const collect = useSelector((y) => y);
+
+  console.log(collect);
+  const show = useDispatch();
+
+  const Increment = () => {
+    show(plus());
+  };
+  const Decriment = () => {
+    show(Minus());
+  };
+  const MinusCounter = () => {
+    show(AsncMinus(4));
+  };
+
+  return (
+    <div>
+      <h1>{collect}</h1>
+      <br />
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decriment}>Decriment</button>
+      <button onClick={MinusCounter}>Multy</button>
+    </div>
+  );
+}
